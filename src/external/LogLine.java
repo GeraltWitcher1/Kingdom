a77ac3addc496a1077083eb9b2a1eb3595ff1ddc
Ion-Caus <causion01@gmail.com>
2021-05-04 10:50:19 +0300

    first commit

---
+++ src/external/LogLine.java
@@ -0,0 +1,26 @@
+package external;
+
+import model.DateTime;
+
+public class LogLine {
+    private DateTime dateTime;
+    private String text;
+
+    public LogLine(String text) {
+        this.text = text;
+        this.dateTime = new DateTime();
+    }
+
+    public DateTime getDateTime() {
+        return dateTime;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s: %s", dateTime, text);
+    }
+}
---
+++ src/external/LogTest.java
@@ -0,0 +1,7 @@
+package external;
+
+public class LogTest {
+    public static void main(String[] args){
+        Log.getLog().addLog("Hello");
+    }
+}
